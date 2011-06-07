72bc6157fd6276911f0361ceaa53823d5f9d856a
rperez <rperez@1cfd4a00-6b3e-4059-ac93-9806997b0ed0>
2011-06-07 00:41:40 +0000

    Updated setup to 1.0.1 release.
    git-svn-id: http://svn.pyopt.org/trunk@6 1cfd4a00-6b3e-4059-ac93-9806997b0ed0

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
     from numpy.distutils.core import setup
     setup(
         name             = 'pyOpt',
-        version          = '1.0.0',
+        version          = '1.0.1',
         author           = 'Ruben E. Perez, Peter W. Jansen',
         author_email     = 'Ruben.Perez@rmc.ca; Peter.Jansen@rmc.ca',
         maintainer       = 'pyOpt Developers',
