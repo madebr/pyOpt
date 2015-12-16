261a5e7d3cd52bea35ad4588a563145b8551591e
Anonymous Maarten <anonymous.maarten@gmail.com>
2015-12-16 16:59:51 +0100

    2to3: fix exec

--- pyOpt/__init__.py
+++ pyOpt/__init__.py
@@ -17,7 +17,7 @@ dir = os.path.dirname(os.path.realpath(__file__))
 for f in os.listdir(dir):
     if f.startswith('py') and os.path.isdir(os.path.join(dir,f)):
         try:
-            exec 'from %s import %s' %(f,f.strip('py'))
+            exec('from %s import %s' %(f,f.strip('py')))
             __all__.extend(sys.modules['pyOpt.'+f].__all__)
         except:
             continue
