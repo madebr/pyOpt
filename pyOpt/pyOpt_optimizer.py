a02a8dc7d3c5e5ad344149d913cd30e7131d4337
LiuKai <liukaiv5@gmail.com>
2020-05-12 14:42:18 +0800

    change dict_keys.sort() to sorted(dict_keys)

--- pyOpt/pyOpt_optimizer.py
+++ pyOpt/pyOpt_optimizer.py
@@ -370,8 +370,7 @@ def ListAttributes(self):
         print('\n')
         print('Attributes List of: ' + repr(self.__dict__['name']) + ' - ' + self.__class__.__name__ + ' Instance\n')
         self_keys = self.__dict__.keys()
-        self_keys.sort()
-        for key in self_keys:
+        for key in sorted(self_keys):
             if key != 'name':
                 print(str(key) + ' : ' + repr(self.__dict__[key]))
 
